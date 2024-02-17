198f8cd0bf5e3dd5aa49fdbe4c925f009d0fd27d
FeodorPyth <grebnevfedork@yandex.ru>
2024-02-17 19:59:14 +0300

    Modernize .env

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,20 +1,17 @@
 # flake8: noqa
 import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = os.environ.get('DEBUG').lower() == 'true'
 
-ALLOWED_HOSTS = [
-    '*',
-    '51.250.104.171',
-    '127.0.0.1',
-    'localhost',
-    'kittygramspb.serveblog.net',
-]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
